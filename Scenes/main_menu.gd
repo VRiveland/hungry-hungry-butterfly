aa0ec011938147b4366a3738d55e6e82b43a1996
Vilde Riveland <jeg.heter.vilde@gmail.com>
2024-02-06 09:38:32 +0100

    Added quit button

--- Scenes/main_menu.gd
+++ Scenes/main_menu.gd
@@ -2,15 +2,9 @@ extends Control
 
 @onready var game_scene = preload("res://Scenes/game.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_packed(game_scene)
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
